082acca59132657799ab0830255e494a606a2956
Vanessacor <vanessapcbarros@gmail.com>
2020-08-19 15:31:32 +0200

    add mail icon to footer with mailgo, add sections to homepage and fix width on homepage

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "mailgo/dist/mailgo.min.js";
 
 import "./style.scss";
 
@@ -14,6 +15,16 @@ function Footer() {
         <li className="Footer-media-item">
           <a href="https://github.com/vanessacor" target="_blank">
             <i className="icon ion-logo-github"></i>
+          </a>
+        </li>
+        <li className="Footer-media-item">
+          <a
+            className="dark"
+            href="#mailgo"
+            data-address="vanessapcbarros"
+            data-domain="gmail.com"
+          >
+            <i className="icon ion-md-mail"></i>
           </a>
         </li>
       </ul>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 
 import "./style.scss";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="Home">
-      <h2>
-        An <span className="Home-strong">avid </span>
-        <span className="Home-highlight">Web</span>{" "}
-        <span className="Home-highlight"> Dev</span> learner looking for
-        <span className="Home-strong"> her role</span> in the web industry.
-      </h2>
+      <div className="Home-intro">
+        <h2>
+          An <span className="Home-intro-strong">avid </span>
+          <span className="Home-intro-highlight">Web</span>{" "}
+          <span className="Home-intro-highlight"> Dev</span> learner looking for
+          <span className="Home-intro-strong"> her role</span> in the web
+          industry.
+        </h2>
+      </div>
+      <div className="Home-sections">
+        <section>
+          <Link to="/about">About</Link>
+        </section>
+        <section>
+          <Link to="/portfolio">Portfolio </Link>
+        </section>
+        <section>
+          <Link to="/work">Work</Link>
+        </section>
+      </div>
     </div>
   );
 }
