e05cabc5689202585626807fb7f8126e8cb9d567
vanessa <vanessapcbarros@gmail.com>
2021-05-20 09:23:49 +0200

    clean: remove a comment on savefile.js

--- build/saveFile.js
+++ build/saveFile.js
@@ -2,7 +2,6 @@ const fsp = require('fs').promises;
 
 const saveFile = (data, file) => {
   const dataToJson = JSON.stringify(data);
-  // const file = "experiment2.json";
   const ps = fsp.writeFile(file, dataToJson);
 
   return ps;
